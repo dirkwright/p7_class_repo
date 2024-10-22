d72a7ed628c87022b429a3d5f7537a1c0dd86682
Nal3 <nalcal321@gmail.com>
2024-10-22 15:07:51 -0700

    added my name to the rost and my two truths and one lie method

--- roster.py
+++ roster.py
@@ -1,10 +1,16 @@
 # File: roster.py
 class Classroom:
     def __init__(self):
-        self.roster = ['student1']  # Initial roster, add your name here
+        self.roster = ['student1', 'Nathaniel Brundjar']  # Initial roster, add your name here
 
     def two_truths_one_lie(self):
         print("Here are two truths and one lie about student1:")
         print("1. I can speak three languages.")
         print("2. I have been to space.")
         print("3. I have a pet cat.")
+
+    def two_truths_one_lie_nathaniel(self):
+        print("Here are two truths and one lie about Nathaniel:")
+        print("1. I have have played in carneghie hall")
+        print("2. I have been to mexico")
+        print("3. One of those is a lie")
